fc1dc78df00ec9def5470049904e2e284cb2e40e
BingyuanXu <xubingyuan123@hotmail.com>
2020-11-20 03:09:06 -0600

    fix model

--- BugTracker/Models/Project.cs
+++ BugTracker/Models/Project.cs
@@ -9,5 +9,6 @@ namespace BugTracker.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<ProjectUser> projectUsers { get; set; }
     }
 }
--- BugTracker/Models/Ticket.cs
+++ BugTracker/Models/Ticket.cs
@@ -14,27 +14,21 @@ namespace BugTracker.Models
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime Updated { get; set; } 
 
-        [Foreignkey("Project")] 
         public string ProjectId { get; set; }
         public virtual Project Project { get; set; }
 
-        [Foreignkey("TicketType")]
         public int TicketTypeId { get; set; }
         public virtual TicketType TicketType { get; set; }
 
-        [Foreignkey("TicketStatus")]
         public int TicketStatusId { get; set; }
         public virtual TicketStatus TicketStatus { get; set; }
 
-        [Foreignkey("TicketPriority")]
         public int TicketPriorityId { get; set; }
         public virtual TicketPriority TicketPriority { get; set; }
 
-        [Foreignkey("OwnerUser")]
         public string OwnerUserId { get; set; }
         public virtual ApplicationUser OwnerUser { get; set; }
 
-        [Foreignkey("AssignedToUserId")]
         public string AssignedToUserId { get; set; }
         public virtual ApplicationUser AssignedToUser{ get; set; }
     }
